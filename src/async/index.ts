50e49fb7d21e3f02a3a44e094a642168e96f6b7f
ShlomoNus <shlomonuswork@gmail.com>
2024-02-10 22:10:27 +0200

    with AsyncResult

---
+++ src/async/index.ts
@@ -0,0 +1,19 @@
+type AsyncResult<TData> = Promise<
+    | {
+          status: true;
+          data: TData;
+      }
+    | {
+          status: false;
+          error: any;
+      }
+>;
+
+export async function asyncWrapper<T>(promise: Promise<T>): AsyncResult<T> {
+    try {
+        const data = await promise;
+        return { status: true, data };
+    } catch (error) {
+        return { status: false, error };
+    }
+}
